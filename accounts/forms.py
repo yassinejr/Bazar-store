3eeafcb5b35f7a638d77d761726fc2fa21c06940
yassine <yassine.jrad@esprit.tn>
2020-11-26 14:41:43 +0100

    add signup

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,12 +1,33 @@
 from django import forms
 from .models import Profile
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 
 
-class UserForm(forms.ModelForm):
+class UserForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
     class Meta:
         model = User
-        fields = ['username', 'first_name', 'last_name', 'email']
+        fields = [
+            'first_name',
+            'last_name',
+            'email',
+            'username',
+            'password1',
+            'password2',
+        ]
+
+    def save(self, commit=True):
+        user = super(UserForm, self).save(commit=True)
+        user.first_name = self.cleaned_data.get('first_name')
+        user.last_name = self.cleaned_data.get('last_name')
+        user.email = self.cleaned_data.get('email')
+
+        if commit:
+            user.save()
+
+        return user
 
 
 class ProfileForm(forms.ModelForm):
--- accounts/views.py
+++ accounts/views.py
@@ -2,23 +2,24 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm, PasswordChangeForm
 from .models import Profile
 from django.contrib.auth import login, authenticate
-from .forms import *
+from .forms import UserForm
 
 
 # Create your views here.
 def signup(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+    if request.method == "POST":
+        form = UserForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            password = form.cleaned_data.get('password')
+            password = form.cleaned_data.get('password1')
 
             user = authenticate(username=username, password=password)
             login(request, user)
             return redirect('/')
+
     else:
-        form = UserCreationForm()
+        form = UserForm()
 
     context = {'form': form}
     return render(request, 'registration/signup.html', context)
