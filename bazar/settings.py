1a24b62a38ee86e8a40e0c23db644a9874a51def
yassine <yassine.jrad@esprit.tn>
2020-11-30 15:12:32 +0100

    req file update & debug turned to false

--- bazar/settings.py
+++ bazar/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'f=&q9_94r_cjnfsi3)pr74urw@y%oygh@d693jf#_o*@ts)rm('
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
